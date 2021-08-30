88217be6cf73a4c105619978c3facd5876ac144b
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2021-08-30 19:20:23 +0430

    inheritance add

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,51 @@
 // Code goes here!
 
-class Departeman {
+class Department {
     name : string ;
-    // employees :  string[]= []
-    constructor (n : string){
+    employees :  string[]= []
+    constructor (private id:string,n : string){
         this.name = n ;
 
     }
-    describe(this:Departeman){
-        console.log("departmen" +  this.name)
+    describe(this:Department){
+        console.log("departmen" +  this.name + "_" + this.id)
+    }
+    addEmployee(employee: string) {
+        
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-const accounting = new Departeman("accounting") ;
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+      super(id, 'Accounting');
+    }
+  
+    addReport(text: string) {
+      this.reports.push(text);
+    }
+  
+    printReports() {
+      console.log(this.reports);
+    }
+  }
+const accounting = new Department("d1","accounting") ;
+const it = new AccountingDepartment("it" , ["this new apper"])
+it.addReport("kia is bala")
+it.printReports();
 
-accounting.describe();
-const copyAccount = {name: "DYMMY" , describe :  accounting.describe} ;
-copyAccount.describe()
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+
+//access to employee only after add make obj ---after change accessModifier private---------
+// accounting.employees[2] = "anne"
+// accounting.addEmployee("anneNosia")
+
+
+// accounting.describe();
+// accounting.printEmployeeInformation();
